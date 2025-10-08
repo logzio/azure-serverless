d66e631ed3fecc6b50977ef60112652ed74d9b5d
ralongit <ralgotmail@gmail.com>
2025-10-08 14:19:17 +0300

    Fix attrs

--- deployments/azuredeploylogs.tf
+++ deployments/azuredeploylogs.tf
@@ -33,7 +33,7 @@ resource "null_resource" "wait_for_namespace" {
 # Data source for authorization rule
 data "azurerm_eventhub_namespace_authorization_rule" "example" {
   name                = "RootManageSharedAccessKey"
-  namespace_id      = azurerm_eventhub_namespace.eventhub_namespace.id
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
   resource_group_name = var.resource_group_name
 
   # Enforce dependency via null_resource
@@ -69,7 +69,6 @@ resource "azurerm_storage_container" "storage_container" {
 resource "azurerm_eventhub" "event_hub" {
   name                = var.eventhub_logs_name
   namespace_id      = azurerm_eventhub_namespace.eventhub_namespace.id
-  resource_group_name = var.resource_group_name
   partition_count     = 32
   message_retention   = 7
 }
@@ -90,7 +89,7 @@ resource "azurerm_linux_function_app" "function_app" {
   resource_group_name        = var.resource_group_name
 
   service_plan_id            = azurerm_service_plan.app_service_plan.id
-  storage_account_id       = azurerm_storage_account.storage_account.id
+  storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
 
   site_config {
